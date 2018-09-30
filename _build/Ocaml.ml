8daaf26a1e5f72a36c91b5f9d450355a7d874bde
Qasimtalkin <abulqasim110@gmail.com>
2018-09-30 19:42:15 -0400

    changes

---
+++ _build/Ocaml.ml
@@ -0,0 +1,9 @@
+open Core.Std 
+
+let rec  read_and_accumulate accum = 
+  let line = In_channel.input_line In_channel.stdin in 
+  match line with 
+    | None -> accm
+    | Some -> read_and_accumulate (accum +. Float.of_string x)
+
+  let () = printf "Total:  %F\n" (read_and_accumulate 0.)
